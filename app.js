56b51a53037fc51351a539a2997a16ba63819198
mora-fs <mora.fernandezstickar@gmail.com>
2021-06-17 19:39:50 -0300

    correción del path de /product

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.listen(3000, () => {
     console.log('Servidor corriendo en el puerto 3000')
 })
 
-app.get('/', (req,res)=>{
+app.get('/product', (req,res)=>{
     res.sendFile(path.resolve(__dirname, './views/product.html'))
 })
 
