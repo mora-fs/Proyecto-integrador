ba5ef74b2593ddbffe69c1c2b330b3f58332b363
bautistallobeta <bautillobeta@gmail.com>
2021-09-06 14:54:40 -0300

    empece el crud de registro y añadi logo al header

--- routes/routesUsers.js
+++ routes/routesUsers.js
@@ -2,6 +2,8 @@ const express= require('express');
 const router = express.Router();
 const controller = require('../controllers/userController');
 
-router.get('/miCarrito', controller.shoppingCart)
+// router.get('/miCarrito', controller.shoppingCart)
+
+router.get('/register')
 
 module.exports = router;
