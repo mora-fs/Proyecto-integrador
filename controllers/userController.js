61e7b1376130acb7288f287ee020ce6617c43c2c
bautistallobeta <bautillobeta@gmail.com>
2021-09-26 21:35:48 -0300

    Termine la logica de agregar productos al carrito

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,13 +4,41 @@ const path= require('path');
 const productsDbPath= path.join(__dirname, '../data/productsDataBase.json');
 const parsedProductsDb= JSON.parse(fs.readFileSync(productsDbPath, 'utf-8'));
 
+
+const session = require('express-session');
+
 const controller= {
     cart: (req, res) => {
-        const productos= {
-            productos: parsedProductsDb
+        if(req.session.productsOnCart){
+            productos = req.session.productsOnCart
+        }
+        else{
+            productos = undefined
         }
         return res.render('shoppingCart', productos)
     },
+
+    addToCart: (req,res) => {
+        const idDetail= req.params.id;
+        const productToAdd= parsedProductsDb.find(product=> product.id == idDetail);
+
+        if(req.session.productsOnCart){
+            req.session.productsOnCart.push(productToAdd)
+        }
+        else{
+            req.session.productsOnCart = []
+            
+            req.session.productsOnCart.push(productToAdd)
+        }
+
+
+
+
+        redirectionRoute = '/productos/' + idDetail;
+        res.redirect(redirectionRoute)
+    
+    },
+
     profile: (req, res) => {
         return res.render('profile')
     }
--- routes/routesUsers.js
+++ routes/routesUsers.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const controller = require('../controllers/userController');
 const hideFromGuestMiddleware= require('../middlewares/hideFromGuestMiddleware');
 
-router.get('/cart', hideFromGuestMiddleware, controller.cart)
+router.get('/cart', /*hideFromGuestMiddleware,*/ controller.cart)
+router.post('/cart/:id', /*hideFromGuestMiddleware,*/ controller.addToCart)
 router.get('/profile', hideFromGuestMiddleware, controller.profile)
 
 module.exports = router;
