1ae8fc8356322b7dfe5315e1ca0ab09f75035aa3
bautistallobeta <bautillobeta@gmail.com>
2021-09-28 11:34:49 -0300

    carousel slider terminado y corregidas algunas rutas viejas

--- routes/routesUsers.js
+++ routes/routesUsers.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const controller = require('../controllers/userController');
 const hideFromGuestMiddleware= require('../middlewares/hideFromGuestMiddleware');
 
-router.get('/cart', hideFromGuestMiddleware, controller.cart)
+router.get('/cart',  controller.cart)
 router.post('/cart/:id', controller.addToCart)
 router.post('/cart/:id/delete', controller.deleteFromCart)
 router.get('/profile', hideFromGuestMiddleware, controller.profile)
