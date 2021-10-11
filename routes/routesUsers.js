417708a4e6a78eb30553b783ee7baae94e53f593
bautistallobeta <bautillobeta@gmail.com>
2021-10-11 19:58:14 -0300

    crud de login hecho, logout tambien hecho

--- routes/routesUsers.js
+++ routes/routesUsers.js
@@ -7,5 +7,6 @@ router.get('/cart', hideFromGuestMiddleware,  controller.cart)
 router.post('/cart/:id', hideFromGuestMiddleware, controller.addToCart)
 router.post('/cart/:id/delete',  controller.deleteFromCart)
 router.get('/profile', hideFromGuestMiddleware, controller.profile)
+router.post('/logout', controller.logOut)
 
 module.exports = router;
